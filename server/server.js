9c0e66965bca7b2c5fe89a4d204c5e0afce9f9a9
Kalman Web Design <166734153+KalmanWebDesign@users.noreply.github.com>
2024-06-10 08:09:26 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -71,7 +71,7 @@ app.post("/create-checkout-session", async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
